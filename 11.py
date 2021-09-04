d8bc578fb8317c8826f21ff4bb6bbeb2b24dd573
jean vega <jeanv046@gmail.com>
2021-09-04 17:49:03 -0500

    undecimo punto ok

---
+++ 11.py
@@ -0,0 +1,9 @@
+presupuesto = int(input("Cual es el presupuesto total: "))
+
+ginecologia = (presupuesto * (40 / 100))
+traumatologia = (presupuesto * (30 / 100))
+pediatria = (presupuesto * (30 / 100))
+
+print("Ginecologia recibira: $", ginecologia)
+print("Traumatologia recibira: $", traumatologia)
+print("Pediatria recibira: $", pediatria)
