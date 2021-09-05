033465cac403da731c1c365ab80ad78cd396d6ab
jean vega <jeanv046@gmail.com>
2021-09-04 19:09:35 -0500

    8 ok

---
+++ 9.py
@@ -0,0 +1,13 @@
+minicial = int(input("Cual es el monto incial? : "))
+mfinal = int(input("Cual es el monto final? : "))
+
+
+if minicial > mfinal:
+    resta = mfinal - minicial
+    resta = resta * -1
+    total = (resta * (20 / 100))
+    suma = resta + total
+    print("Costo de la llamada: $", suma)
+else: print("digite los montos adecuados.")
+
+
