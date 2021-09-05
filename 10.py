fb726d1d6832e10e6bdb600088c03cf378f224f2
jean vega <jeanv046@gmail.com>
2021-09-04 19:10:37 -0500

    10 ok

---
+++ 10.py
@@ -0,0 +1,9 @@
+revelado = int(input("Cuantas fotos se van a revelar? : "))
+
+total = revelado * 1500
+
+porc = (total * (16 /100))
+
+total1 = total + porc
+
+print("Monto a pagar por el revelado completo es de: $", total1)
